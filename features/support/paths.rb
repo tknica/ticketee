78aa28d63428849eb272bf78e82c2ac45f83bb2b
Your Name <surfaround@web.de>
2012-04-21 01:29:12 +0200

    Added title functionality for show page

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,7 +10,6 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
-
     when /the project page for "([^\"]*)"/
       project_path(Project.find_by_name!($1))
     # Add more mappings here.
