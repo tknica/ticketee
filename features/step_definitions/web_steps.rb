3ff87896af1f955a3d774f1322fb215b813816a2
myhost <myhost@localhost.(none)>
2012-05-28 11:53:33 +0200

    Implemented features for displaying a list of relevant tickets for projects and viewing particular tickets

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -5,7 +5,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "sel
 
 module WithinHelpers
   def with_scope(locator)
-    locator ? within(*selector_for(locator)) { yield } : yield
+    locator ? within(locator) { yield } : yield
   end
 end
 World(WithinHelpers)
@@ -26,27 +26,108 @@ When /^(?:|I )press "([^"]*)"$/ do |button|
   click_button(button)
 end
 
-Then /^(?:|I )should see "([^"]*)"$/ do |text|
-  if page.respond_to? :should
-    page.should have_content(text)
-  else
-    assert page.has_content?(text)
+Then /^(?:|I )should see "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, selector|
+  with_scope(selector) do
+    if defined?(Spec::Rails::Matchers)
+      page.should have_content(text)
+    else
+      assert page.has_content?(text)
+    end
   end
 end
 
-Then /^(?:|I )should not see "([^"]*)"$/ do |text|
-  if page.respond_to? :should
-    page.should have_no_content(text)
-  else
-    assert page.has_no_content?(text)
+Then /^(?:|I )should see \/([^\/]*)\/(?: within "([^\"]*)")?$/ do |regexp, selector|
+  regexp = Regexp.new(regexp)
+  with_scope(selector) do
+    if defined?(Spec::Rails::Matchers)
+      page.should have_xpath('//*', :text => regexp)
+    else
+      assert page.has_xpath?('//*', :text => regexp)
+    end
+  end
+end
+
+Then /^(?:|I )should not see "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, selector|
+  with_scope(selector) do
+    if defined?(Spec::Rails::Matchers)
+      page.should have_no_content(text)
+    else
+      assert page.has_no_content?(text)
+    end
+  end
+end
+
+Then /^(?:|I )should not see \/([^\/]*)\/(?: within "([^\"]*)")?$/ do |regexp, selector|
+  regexp = Regexp.new(regexp)
+  with_scope(selector) do
+    if defined?(Spec::Rails::Matchers)
+      page.should have_no_xpath('//*', :text => regexp)
+    else
+      assert page.has_no_xpath?('//*', :text => regexp)
+    end
+  end
+end
+
+Then /^the "([^\"]*)" field(?: within "([^\"]*)")? should contain "([^\"]*)"$/ do |field, selector, value|
+  with_scope(selector) do
+    if defined?(Spec::Rails::Matchers)
+      find_field(field).value.should =~ /#{value}/
+    else
+      assert_match(/#{value}/, field_labeled(field).value)
+    end
+  end
+end
+
+Then /^the "([^\"]*)" field(?: within "([^\"]*)")? should not contain "([^\"]*)"$/ do |field, selector, value|
+  with_scope(selector) do
+    if defined?(Spec::Rails::Matchers)
+      find_field(field).value.should_not =~ /#{value}/
+    else
+      assert_no_match(/#{value}/, find_field(field).value)
+    end
+  end
+end
+
+Then /^the "([^\"]*)" checkbox(?: within "([^\"]*)")? should be checked$/ do |label, selector|
+  with_scope(selector) do
+    if defined?(Spec::Rails::Matchers)
+      find_field(label)['checked'].should == 'checked'
+    else
+      assert_equal 'checked', field_labeled(label)['checked']
+    end
+  end
+end
+
+Then /^the "([^\"]*)" checkbox(?: within "([^\"]*)")? should not be checked$/ do |label, selector|
+  with_scope(selector) do
+    if defined?(Spec::Rails::Matchers)
+      find_field(label)['checked'].should_not == 'checked'
+    else
+      assert_not_equal 'checked', field_labeled(label)['checked']
+    end
   end
 end
 
 Then /^(?:|I )should be on (.+)$/ do |page_name|
-  current_path = URI.parse(current_url).path
-  if current_path.respond_to? :should
-    current_path.should == path_to(page_name)
+  if defined?(Spec::Rails::Matchers)
+    URI.parse(current_url).path.should == path_to(page_name)
   else
-    assert_equal path_to(page_name), current_path
+    assert_equal path_to(page_name), URI.parse(current_url).path
   end
+end
+
+Then /^(?:|I )should have the following query string:$/ do |expected_pairs|
+  actual_params   = CGI.parse(URI.parse(current_url).query)
+  expected_params = Hash[expected_pairs.rows_hash.map{|k,v| [k,[v]]}]
+
+  if defined?(Spec::Rails::Matchers)
+    actual_params.should == expected_params
+  else
+    assert_equal expected_params, actual_params
+  end
+end
+
+Then /^show me the page$/ do
+  p body
+  save_and_open_page
 end
